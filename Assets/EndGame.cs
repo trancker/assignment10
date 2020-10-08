94e7acbcac4570681c315d7867bf3f7ee231f094
Mohammad Tanzil Idrisi <44720341+tanzil786@users.noreply.github.com>
2020-10-08 15:05:17 +0530

    Initial commit

---
+++ Assets/EndGame.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGame : MonoBehaviour
+{
+    public Boolean hit = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        hit = true;
+    }
+}
---
+++ Assets/EndText.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class EndText : MonoBehaviour
+{
+    public GameObject BoxCollider;
+    private Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+
+        // start text off as completely transparent black
+        text.color = new Color(0, 0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(BoxCollider.GetComponent<EndGame>().hit == true)
+        {
+            text.color = new Color(1, 0, 0, 1);
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                // reload entire scene, starting music over again, refreshing score, etc.
+                SceneManager.LoadScene("Level");
+            }
+        }
+    }
+}
